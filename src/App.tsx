4dcb8977bb60e07899763d01b55dd9db8fde02b3
christinapan98 <christina.pan@wustl.edu>
2025-01-08 12:04:58 -0500

    Add body text, contact form, video autoplay, and footer placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import './App.css';
 import Header from './components/Header';
-import SectionOneVideo from './videos/Liberata_Section_One.mp4';
-import ReactPlayer from 'react-player/lazy';
+// import SectionOneVideo from './videos/Liberata_Section_One.mp4';
+import SectionOneVideo from './videos/Liberata_Section_One_White.mp4'
+import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
   const [isMeritVideoOn, setMeritVideoOn] = useState(false);
   const [isEconomicVideoOn, setEconomicVideoOn] = useState(false);
   const [isTrustVideoOn, setTrustVideoOn] = useState(false);
+  const videoRef = useRef(null);
+  let shouldAutoplay = true;
 
+  // Section observer
   useEffect(() => {
-    // Section observer
     const sectionItems = document.getElementsByClassName("App-section");
     const observerCallback = (entries: any[]) => {
       entries.forEach((entry) => {
@@ -27,6 +31,25 @@ function App() {
     });
   }, []);
 
+  // Overview video observer
+  useEffect(() => {
+    let myVideoRef = videoRef.current || null;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && myVideoRef && shouldAutoplay) {
+            (myVideoRef as HTMLVideoElement).play();
+            shouldAutoplay = false;
+          } else if(!entry.isIntersecting && myVideoRef) {
+            (myVideoRef as HTMLVideoElement).pause();
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+    if(myVideoRef) observer.observe(myVideoRef);
+  }, []);
+
   return (
     <div className="App">
       <div className="Header-accent"></div>
@@ -34,25 +57,23 @@ function App() {
       <div className="App-body">
         {/* Hook and overview */}
         <div className="App-section" id="section-one">
-          dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. 
+          Introducing Project Liberata. 
         </div>
 
-        <div className="App-section">
-          <ReactPlayer
-            url={SectionOneVideo}
-            controls={true}
-            width="1000"
-            height="500"
-            id="section-one-video"
-          ></ReactPlayer>
+        <div className="App-section App-body-section">
+          <video ref={videoRef} src={SectionOneVideo} width="100%" 
+          id="section-one-video"
+          controls muted/>
         </div>
 
         {/* Three problems */}
-        <div className="App-section App-problem" id="section-two">
-          <div className="App-problem-header sticky">Solving the merit problem</div>
+        <div className="App-section App-body-section App-problem" id="section-two">
+          <div className="App-problem-header sticky">
+            Addressing the merit problem
+          </div>
           <div className="App-problem-body">
             <div>
-              Eliminating corruption and politics within academia
+            Ending the politics of maneuvering for authorship positions
             </div>
             <div className="App-problem-button-carousel">
               <button className={isMeritVideoOn ? 'activeButton' : ''} onClick={() => setMeritVideoOn(true)}>Video</button>
@@ -60,16 +81,23 @@ function App() {
             </div>
             <div>
               <div style={{display: isMeritVideoOn ? 'block' : 'none'}}>Video placeholder dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. </div>
-              <div style={{display: !isMeritVideoOn ? 'block' : 'none'}}>Text placeholder dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. </div>
+              <div style={{display: !isMeritVideoOn ? 'block' : 'none'}}>
+                <p>
+                The merit problem concerns how scientific contributions are recognized within academia.
+                </p>
+                <p>
+                In the current system, an author’s position on a paper is imprecisely correlated to their contributions on that paper. For example, the first author position is typically considered disproportionally more prestigious than other positions regardless of other authors’ contributions. This system is prone to political maneuvering within labs and institutions as researchers compete for authorship positions. Additionally, there is no recognizable form of credit given to other key roles within the scientific process, such as to peer reviewers and study replicators.
+                </p>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="App-section App-problem">
-          <div className="App-problem-header sticky">Solving the economic problem</div>
+        <div className="App-section App-body-section App-problem">
+          <div className="App-problem-header sticky">Addressing the economic problem</div>
           <div className="App-problem-body">
             <div>
-              Eliminating reward mismatch
+            Eliminating reward mismatch among the academic publishing community
             </div>
             <div className="App-problem-button-carousel">
               <button className={isEconomicVideoOn ? 'activeButton' : ''} onClick={() => setEconomicVideoOn(true)}>Video</button>
@@ -77,16 +105,23 @@ function App() {
             </div>
             <div>
               <div style={{display: isEconomicVideoOn ? 'block' : 'none'}}>Video placeholder dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. </div>
-              <div style={{display: !isEconomicVideoOn ? 'block' : 'none'}}>Text placeholder dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. </div>
+              <div style={{display: !isEconomicVideoOn ? 'block' : 'none'}}>
+                <p>
+                The economic problem concerns how the proceeds from a work of research is split up among the parties involved in the publishing process. After funding is secured, typically via taxpayer-backed government grants, a key mismatch arises: while researchers do most of the work, private publishing companies collect most of the revenue from publications through subscription and publishing fees.
+                </p>
+                <p>
+                Though publishers’ fees are justified by their content curation, this service is still done by a volunteer-based peer review effort from academics that costs publishers very little. This system could be made more effective if fees could be eliminated while preserving quality. Current open-source publishing platforms have removed fees but haven't yet developed strong quality control mechanisms.
+                </p>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="App-section App-problem">
-          <div className="App-problem-header sticky">Solving the trust problem</div>
+        <div className="App-section App-body-section App-problem">
+          <div className="App-problem-header sticky">Addressing the trust problem</div>
           <div className="App-problem-body">
             <div>
-              Targeting the replication crisis
+            Paving the way for more reliable research by incentivizing the replication of scientific studies
             </div>
             <div className="App-problem-button-carousel">
               <button className={isTrustVideoOn ? 'activeButton' : ''} onClick={() => setTrustVideoOn(true)}>Video</button>
@@ -94,18 +129,27 @@ function App() {
             </div>
             <div>
               <div style={{display: isTrustVideoOn ? 'block' : 'none'}}>Video placeholder dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. </div>
-              <div style={{display: !isTrustVideoOn ? 'block' : 'none'}}>Text placeholder dolor sit amet, consectetur elit, sed do tempor incididunt ut labore. </div>
+              <div style={{display: !isTrustVideoOn ? 'block' : 'none'}}>
+                  <p>
+                  This section concerns how the body of scientific literature can be made more robust and reliable. At present, the vast majority of scientific findings are supported only by a single study, due to a lack of incentive for replication studies. Because of this, for most research fields, it can be difficult for later works to detect errors in preceding works. The ever increasing scale of academic research only exacerbates this issue and has noticeably affected public trust in science.
+
+                  Solving this problem entails having a mechanism for incentivizing replication studies that is robust to collusion.
+                </p>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="App-section" id="section-three">
-          Video Playlist
+        <div className="App-section App-body-section" id="section-three">
+          Introducing the shareholder model
         </div>
-        <div className="App-section" id="section-four">
-          FAQ
+
+        <div className="App-section" style={{height: '50vh'}}>
+          <Contact/>
         </div>
       </div>
+      <Footer/>
+      <div className="Footer-accent"></div>
     </div>
   );
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import './Contact.css';
+import TextField from '@mui/material/TextField';
+
+function Contact() {
+  const [emailInputValue, setEmailInputValue] = useState("");
+  const [fNameInputValue, setFNameInputValue] = useState("");
+  const [lNameInputValue, setLNameInputValue] = useState("");
+  const [isSubmitVisible, setSubmitVisible] = useState(false);
+
+  const handleContactFormSubmit = (e) => {
+    e.preventDefault();
+    alert("you've submitted the form");
+    // perform regex checks
+    if(!validateFirstName(fNameInputValue) || !validateLastName(lNameInputValue) || !validateEmail(emailInputValue)) {
+      return;
+    }
+    setEmailInputValue("");
+    setFNameInputValue("");
+    setLNameInputValue("");
+  }
+
+  const validateFirstName = (input : string) => {
+    console.log("validating fname ", input);
+    return true;
+  }
+
+  const validateLastName = (input : string) => {
+    console.log("validating lname ", input);
+    return true;
+  }
+
+  const validateEmail = (input : string) => {
+    console.log("validating email ", input);
+    return true;
+  }
+
+  return (
+    <div className="Contact-body">
+      <div className="Contact-text">
+        <div className="Contact-text-headline">
+          Let's build a better system <span style={{color: 'var(--accent-blue)'}}>together</span>.
+        </div>
+        <div className="Contact-text-subline">
+          Join our mailing list for future updates.
+        </div>
+      </div>
+      <form className="Contact-form" onSubmit={handleContactFormSubmit}>
+        <div style={{display: 'flex', flexDirection: 'row', marginBottom: '13px', gap: '13px'}}>
+          <TextField
+          id="outlined-fname" 
+          label="First name" 
+          value={fNameInputValue} 
+          onChange={(e) => setFNameInputValue(e.target.value)} 
+          onFocus={() => setSubmitVisible(true)}
+          onBlur={() => validateFirstName(fNameInputValue)}/>
+          <TextField 
+          id="outlined-lname" 
+          label="Last name" 
+          value={lNameInputValue} 
+          onChange={(e) => setLNameInputValue(e.target.value)} 
+          onFocus={() => setSubmitVisible(true)}
+          onBlur={() => validateLastName(lNameInputValue)}/>
+        </div>
+        
+        <TextField 
+        id="outlined-email" 
+        label="Email" 
+        value={emailInputValue} 
+        onChange={(e) => setEmailInputValue(e.target.value)} 
+        onFocus={() => setSubmitVisible(true)}
+        onBlur={() => validateEmail(emailInputValue)}/>
+
+        <input type="submit" value="Submit" className={`outlined-submit ${isSubmitVisible && 'is-visible'}`}/>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
