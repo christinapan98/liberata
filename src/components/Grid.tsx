d4de575da4f5618c3378ad0ea64b3c448f53d623
Aarsh Roongta <aarsh.roongta@duke.edu>
2025-08-01 16:22:56 +0530

    practice grid code

---
+++ src/components/Grid.tsx
@@ -0,0 +1,19 @@
+import "./Grid.css"
+
+function Grid() {
+  return (
+    <div className="grid-container">
+      <div className="square">1</div>
+      <div className="square">2</div>
+      <div className="square">3</div>
+      <div className="square">4</div>
+      <div className="square">5</div>
+      <div className="square">6</div>
+      <div className="square">7</div>
+      <div className="square">8</div>
+      <div className="square">9</div>
+    </div>
+  );
+}
+
+export default Grid;
