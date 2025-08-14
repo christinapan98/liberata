58c91fdc4515ab6bfbdf59d8e6d41bcc1c727852
Aarsh Roongta <aarsh.roongta@duke.edu>
2025-08-14 18:22:08 +0530

    More flexible logic for large number of squares

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,17 +1,17 @@
-import "./Grid.css"
+import "./Grid.css";
 
-function Grid() {
+type Prop = {
+  size?: number;
+};
+
+function Grid({ size = 9 }: Prop) {
   return (
     <div className="grid-container">
-      <div className="square">1</div>
-      <div className="square">2</div>
-      <div className="square">3</div>
-      <div className="square">4</div>
-      <div className="square">5</div>
-      <div className="square">6</div>
-      <div className="square">7</div>
-      <div className="square">8</div>
-      <div className="square">9</div>
+      {[...Array(size).keys()].map((n) => (
+        <div className="square" key={n + 1}>
+          {n + 1}
+        </div>
+      ))}
     </div>
   );
 }
